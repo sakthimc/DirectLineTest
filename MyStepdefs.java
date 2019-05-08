1823a09bd000c576630ab2bbf3424a7bd1b50aa8
sakthimc <50382752+sakthimc@users.noreply.github.com>
2019-05-08 11:57:03 +0100

    Added readme and added hooks in step def

---
+++ MyStepdefs.java
@@ -0,0 +1,61 @@
+package stepDefinitions;
+
+import PageObjects.DealerPortalPage;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+public class MyStepdefs {
+    private WebDriver driver;
+    private DealerPortalPage dealerPortalPage;
+
+    @Before
+    public void startUp() {
+        System.setProperty("webdriver.chrome.driver", ".\\chrome\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Given("^I launch the insurance portal$")
+    public void iLaunchTheInsurancePortal() {
+        driver.get("https://covercheck.vwfsinsuranceportal.co.uk/");
+        String pageTitle = driver.getTitle();
+        System.out.println("I am on:" + pageTitle);
+        Assert.assertEquals("PageTitle is:", "Dealer Portal", pageTitle);
+    }
+
+    @When("^I enter the vehicle \"([^\"]*)\" and click submit$")
+    public void iEnterTheVehicleAndClickSubmit(String regNumber) throws Throwable {
+        dealerPortalPage = new DealerPortalPage(driver);
+        if (regNumber.equals("OV12UYY")) {
+            dealerPortalPage.enterValidVehicleRegNum(regNumber);
+        } else {
+            dealerPortalPage.enterValidVehicleRegNum(regNumber);
+            String invalidRegNumberGiven = driver.findElement(By.cssSelector("div#page-container > div.result")).getText();
+            System.out.println(invalidRegNumberGiven);
+        }
+
+    }
+
+    @Then("^I should see the \"([^\"]*)\" insurance details$")
+    public void iShouldSeeTheInsuranceDetails(String regNumber) throws Throwable {
+        if (regNumber.equals("OV12UYY")) {
+            dealerPortalPage = new DealerPortalPage(driver);
+            dealerPortalPage.getVehicleInsuranceDetails();
+            //driver.close();
+        } else {
+            System.out.println("Do nothing");
+           // driver.close();
+        }
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# DirectLineTest
+# DirectLineQA
+This project consists of a simple test for checking whether a vehicle exists in dealer portal.
+# About this project
+I have implemented this project using seleium-cucumber-pageobjects framework. Its pretty light weight so should be easy to set up. 
+The build tool I have used is "Maven". 
+ - feature files can be found under /src/test/resources/features
+ - stepdefinitions can be found under /src/test/java/stepDefinitions
+ - runner class is in /src/test/java/runner
+ 
+# Project setup on local
+1. Git clone this repo onto your local machine
+2. Open the project using your favourite IDE - I used intelliJ Community Edition
+3. Make sure you have chrome browser installed
+
+# Running the project on local machine
+There are two ways you run run this locally 
+1. Right-click on test runner class and run
+2. or you can use mvn clean test
+
